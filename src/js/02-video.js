909a4cea518baab6260a835a12c95c251306f0cb
qasklp <freeee386@gmail.com>
2022-12-25 17:12:57 +0200

    fix video

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,9 +4,10 @@ import throttle from 'lodash.throttle';
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
-const onTimeUpdate = function (data) {
-    localStorage.setItem('videoplayer-current-time', data.seconds);
-};
-player.on('timeUpdate', throttle(onTimeUpdate, 1000));
+function onTimeUpdate(event) {
+    localStorage.setItem("videoplayer-current-time", event.seconds);
+}
+player.on('timeupdate', throttle(onTimeUpdate, 1000));
 
 player.setCurrentTime(localStorage.getItem("videoplayer-current-time") || 0);
+                    
